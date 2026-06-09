810bc36fd76f98a8c78813b288832f804bb75724
DenisAuzyak <deniseywork@gmail.com>
2026-06-09 18:15:34 +0300

    я устал(

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -120,7 +120,7 @@ function positionDropdown() {
       display: none;
     `;
     searchBox.style.position = 'relative';
-    searchBox.appendChild(dropdown);
+    document.body.appendChild(dropdown);
   }
 }
 
